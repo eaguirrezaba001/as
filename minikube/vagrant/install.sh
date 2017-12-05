10b9628d1c0e010b954e6ebe5d54d46bff4d0962
eaguirrezaba001 <eaguirrezaba001@ikasle.ehu.eus>
2017-12-05 18:58:00 +0100

    more changes on minikube vagrant

--- minikube/vagrant/install.sh
+++ minikube/vagrant/install.sh
@@ -6,16 +6,20 @@ apt-get install -y docker.io
 # setup minikube & kubectl 
 curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64 && chmod +x minikube
 curl -Lo kubectl https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl && chmod +x kubectl
+export PATH=$(pwd):$PATH
+source <(kubectl completion bash)
 
-export MINIKUBE_WANTUPDATENOTIFICATION=false
-export MINIKUBE_WANTREPORTERRORPROMPT=false
-export MINIKUBE_HOME=$HOME
-export CHANGE_MINIKUBE_NONE_USER=true
-mkdir $HOME/.kube || true
-touch $HOME/.kube/config
+#export MINIKUBE_WANTUPDATENOTIFICATION=false
+#export MINIKUBE_WANTREPORTERRORPROMPT=false
+#export MINIKUBE_HOME=$HOME
+#export CHANGE_MINIKUBE_NONE_USER=true
+#mkdir $HOME/.kube || true
+#touch $HOME/.kube/config
 
 export KUBECONFIG=$HOME/.kube/config
 sudo -E ./minikube start --vm-driver=none
+sudo chown -R $USER:$USER .minikube/
+sudo chown -R $USER:$USER .kube/
 
 # this for loop waits until kubectl can access the api server that Minikube has created
 for i in {1..150}; do # timeout for 5 minutes
@@ -27,5 +31,13 @@ for i in {1..150}; do # timeout for 5 minutes
 done
 
 # kubectl commands are now able to interact with Minikube cluster
-#kubectl get pods --all-namespaces
-#kubectl cluster-info
+# minikube status
+# kubectl get nodes
+# kubectl get pods
+# kubectl get pods --all-namespaces
+# kubectl get services
+# kubectl cluster-info
+# minikube dashboard
+# minikube dashboard --url
+
+
