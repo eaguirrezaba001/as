ca50f0d0bea57420afba907edbcb024a21ae0190
eaguirrezaba001 <eaguirrezaba001@ikasle.ehu.eus>
2017-12-15 13:00:25 +0100

    Workaround. landrush and docker networking issues

--- gubuntu-minikube/setup.sh
+++ gubuntu-minikube/setup.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
+sudo apt-get install -y whois
 # Gnome
-sudo apt-get install -y gdm gnome-shell whois
+sudo apt-get install -y gdm gnome-shell
 sudo apt-get install -y virtualbox-guest-dkms virtualbox-guest-utils virtualbox-guest-x11
 
 # Utilities
@@ -13,8 +14,21 @@ sudo apt-get install -y docker.io
 useradd -d /home/$1 -m $1 -p `mkpasswd $1`
 usermod -aG sudo $1
 
-# keyboard locale
-#localectl set-keymap es
+# WORKAROUND for private network error
+# https://bugs.launchpad.net/ubuntu/+source/ubuntu-fan/+bug/1729608
+sudo mv /etc/network/fan /etc/network/fan.backup
 
-reboot
+# Workaround for docker networking issues + landrush                                                         
+echo "$(host -t A index.docker.io | grep has.address | head -1 | awk '{print $NF}') index.docker.io" >> /etc/hosts
+echo "$(host -t A registry-1.docker.io | grep has.address | head -1 | awk '{print $NF}') registry-1.docker.io" >> /etc/hosts
+
+
+#keyboard locale
+#localectl set-keymap es
+#setxkbmap -layout us,es
+#setxkbmap es
+#sudo dpkg-reconfigure console-setup
+#sudo dpkg-reconfigure keyboard-configuration
+
+#reboot
 
